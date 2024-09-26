7781161d5f755845d33d3b8885d9b846589241f7
ttntm <ttntm@pm.me>
2024-09-26 12:13:41 +0200

    +feedback

--- src/_includes/js/useSwiper.js
+++ src/_includes/js/useSwiper.js
@@ -13,9 +13,9 @@ window.addEventListener('DOMContentLoaded', () => {
       direction: 'horizontal',
       grabCursor: true,
       loop: true,
-      pagination: {
-        el: '.swiper-pagination'
-      },
+      // pagination: {
+      //   el: '.swiper-pagination'
+      // },
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
